8df5625ccf84dad46602b3e4dd30b51cdd4fe60e
hacker-pyrat <parent.j.f@gmail.com>
2015-10-06 04:44:47 -0400

    New exceptions; docstring phase II;

--- brome/core/model/exceptions.py
+++ brome/core/model/exceptions.py
@@ -7,3 +7,12 @@ class InitDriverException(Exception):
 
     def __init__(self, msg):
         self.msg = msg
+
+class InvalidBrowserName(Exception):
+    """Raise when the given browserName config is invalid
+    Args:
+        msg (str): Human readable string describing the exception.
+    """
+
+    def __init__(self, msg):
+        self.msg = msg

75e1946d64df129680ad3192aa37d2468525e175
hacker-pyrat <parent.j.f@gmail.com>
2015-10-12 16:22:08 -0400

    incrementing version

--- brome/__init__.py
+++ brome/__init__.py
@@ -1,6 +1,6 @@
 #! -*- coding: utf-8 -*-
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 import webbrowser
 import shutil
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt', 'r') as fd:
 
 setup(
     name='brome',
-    version='0.0.7',
+    version='0.0.8',
     description="Framework For Selenium",
     long_description=readme,
     author="Brome-HQ",

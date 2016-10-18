9716fd6a7a85f4904c0cf7eac85f90f92a75a9a7
hacker-pyrat <parent.j.f@gmail.com>
2016-10-18 13:48:11 +0300

    Fix release

--- brome/__init__.py
+++ brome/__init__.py
@@ -21,7 +21,7 @@ from brome.core.configurator import (
     generate_brome_config
 )
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 
 class Brome(object):
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt', 'r') as fd:
 
 setup(
     name='brome',
-    version='1.1.0',
+    version='1.1.1',
     description="Selenium Framework",
     long_description=readme,
     author="Jean-Francois Parent",

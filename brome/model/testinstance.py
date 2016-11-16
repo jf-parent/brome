e83da2f96be9bfb38bb57dd62025a23b22ddea6c
hacker-pyrat <parent.j.f@gmail.com>
2016-11-16 14:31:42 +0300

    :bug: fix typo

--- brome/model/testinstance.py
+++ brome/model/testinstance.py
@@ -29,7 +29,7 @@ class Testinstance(BaseModel):
     log_file_path = StringField(default='')
     network_capture_path = StringField(default='')
     video_capture_path = StringField(default='')
-    video_location = StringField(default='loca')
+    video_location = StringField(default='local')
     # Bot diary
 
     test_batch_id = ObjectIdField()
